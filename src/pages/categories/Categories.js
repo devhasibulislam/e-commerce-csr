c4be603f65c9b6ca5190e352c24ffcdac2108d25
hasibulislam999 <hasib143sl@gmail.com>
2022-11-18 16:33:03 +0600

    categories route init

---
+++ src/pages/categories/Categories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Title from "../../components/Title";
+import Footer from "../../shared/footer/Footer";
+import Navbar from "../../shared/navbar/Navbar";
+
+const Categories = () => {
+  return (
+    <section>
+      <Title>Categories</Title>
+      <Navbar />
+      This is Categories route
+      <Footer />
+    </section>
+  );
+};
+
+export default Categories;
