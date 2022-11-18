47d2fa1c68d40564bd62aea7bae7e4f07fefea98
hasibulislam999 <hasib143sl@gmail.com>
2022-11-18 19:38:53 +0600

    reference file added

---
+++ public/reference.js
@@ -0,0 +1,6 @@
+/**
+ * Git refusing to merge unrelated histories on rebase
+ * https://stackoverflow.com/questions/37937984/git-refusing-to-merge-unrelated-histories-on-rebase
+ * How to Lazy Load Images in React
+ * https://www.freecodecamp.org/news/how-to-lazy-load-images-in-react/
+ */
