132a2e25f0aacd1a4768e0e3a9d79b75f081b1ff
hasibulislam999 <hasib143sl@gmail.com>
2022-11-18 16:33:15 +0600

    forgot route init

---
+++ src/pages/profile/Forgot.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import AccountBanner from "../../components/profile/AccountBanner";
+import AccountButton from "../../components/profile/AccountButton";
+import FormLogo from "../../components/profile/FormLogo";
+import Title from "../../components/Title";
+
+const Forgot = () => {
+  return (
+    <section className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <Title>Forgot password</Title>
+      <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+          <div className="w-fit mx-auto">
+            <FormLogo />
+          </div>
+          <div className="mt-12 flex flex-col items-center">
+            <h1 className="text-2xl xl:text-3xl font-extrabold">
+              Forgot your password
+            </h1>
+            <div className="w-full flex-1">
+              <div className="mx-auto max-w-xs mt-8">
+                <form className="flex flex-col gap-y-4">
+                  {/* email field */}
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                  />
+
+                  {/* password field */}
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="password"
+                    name="password"
+                    placeholder="New Password"
+                  />
+
+                  {/* submit button */}
+                  <AccountButton value={"Forgot Password"} />
+                </form>
+                <p className="mt-6 text-xs text-gray-600 text-center">
+                  Remember your account password?
+                  <Link
+                    to="/sign-in"
+                    className="border-b border-gray-500 border-dotted"
+                  >
+                    {" "}
+                    Login here!
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <AccountBanner />
+      </div>
+    </section>
+  );
+};
+
+export default Forgot;
