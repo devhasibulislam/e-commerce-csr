3a23dbc4a1ca423596616995973d939b9a0af33e
hasibulislam999 <hasib143sl@gmail.com>
2022-11-18 16:32:29 +0600

    product carousel component init

---
+++ src/components/ProductCarousel.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Product from "./Product";
+
+const ProductCarousel = ({ products }) => {
+  // carousel configuration
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+      partialVisibilityGutter: 40,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+      partialVisibilityGutter: 40,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <Carousel
+      responsive={responsive}
+      swipeable={true}
+      draggable={true}
+      arrows={true}
+      infinite={true}
+      className="py-4 discover_carousel"
+    >
+      {products.map((product, index) => (
+        <Product key={index} product={product} />
+      ))}
+    </Carousel>
+  );
+};
+
+export default ProductCarousel;
