86d777ec5c001be79ef85d09db8b368b5149bd52
hasibulislam999 <hasib143sl@gmail.com>
2022-11-18 20:10:08 +0600

    new reference added

--- public/reference.js
+++ public/reference.js
@@ -1,6 +1,10 @@
 /**
  * Git refusing to merge unrelated histories on rebase
  * https://stackoverflow.com/questions/37937984/git-refusing-to-merge-unrelated-histories-on-rebase
+ *
  * How to Lazy Load Images in React
  * https://www.freecodecamp.org/news/how-to-lazy-load-images-in-react/
+ *
+ * You are loading Images wrong! Use this instead
+ * https://www.youtube.com/watch?v=8viWcH5bUE4
  */
