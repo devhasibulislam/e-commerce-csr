27ff453aecc33c22faed9b0b732a55ef55844775
hasibulislam999 <hasib143sl@gmail.com>
2022-11-18 16:32:57 +0600

    blogs route init

---
+++ src/pages/blogs/Blogs.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Title from "../../components/Title";
+import Footer from "../../shared/footer/Footer";
+import Navbar from "../../shared/navbar/Navbar";
+
+const Blogs = () => {
+  return (
+    <section>
+      <Title>Blogs</Title>
+      <Navbar />
+      This is Blogs route
+      <Footer />
+    </section>
+  );
+};
+
+export default Blogs;
