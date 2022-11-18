d326f5174224e057dda664ea05016ee866abc18e
hasibulislam999 <hasib143sl@gmail.com>
2022-11-18 16:33:37 +0600

    home route init

---
+++ src/pages/Home.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Banner from "../components/home/Banner";
+import BestSelling from "../components/home/BestSelling";
+import Blogs from "../components/home/Blogs";
+import Department from "../components/home/Department";
+import Discover from "../components/home/Discover";
+import ExpertsChoice from "../components/home/ExpertsChoice";
+import Motivation from "../components/home/Motivation";
+import NewArrival from "../components/home/NewArrival";
+import Newsletter from "../components/home/Newsletter";
+import Promo from "../components/home/Promo";
+import Reviews from "../components/home/Reviews";
+import StartExploring from "../components/home/StartExploring";
+import Trending from "../components/home/Trending";
+import WorkProcedure from "../components/home/WorkProcedure";
+import Title from "../components/Title";
+import Footer from "../shared/footer/Footer";
+import Navbar from "../shared/navbar/Navbar";
+
+const Home = () => {
+  return (
+    <section>
+      <Title>Home</Title>
+      <Navbar />
+      <Banner />
+      <Discover />
+      <NewArrival />
+      <WorkProcedure />
+      <hr className="container mx-auto" />
+      <Motivation />
+      <StartExploring />
+      <BestSelling />
+      <Promo />
+      <ExpertsChoice />
+      <hr className="container mx-auto" />
+      <Department />
+      <Newsletter />
+      <Trending />
+      <Blogs />
+      <Reviews />
+      <Footer />
+    </section>
+  );
+};
+
+export default Home;
