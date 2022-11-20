2e2d75926aaec4985d437f9923b19f9f1b9feefb
hasibulislam999 <hasib143sl@gmail.com>
2022-11-20 22:36:58 +0600

    dynamically funcitonalize forgot route

--- src/pages/profile/Forgot.js
+++ src/pages/profile/Forgot.js
@@ -1,11 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import AccountBanner from "../../components/profile/AccountBanner";
 import AccountButton from "../../components/profile/AccountButton";
 import FormLogo from "../../components/profile/FormLogo";
 import Title from "../../components/Title";
+import Loading from "../../shared/loading/Loading";
 
 const Forgot = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function handleResetPassword(event) {
+    event.preventDefault();
+
+    const userInformation = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
+
+    const resetPassword = async () => {
+      setLoading(true);
+      const request = await fetch(`http://localhost:8080/user/reset-password`, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(userInformation),
+      });
+      const response = await request.json();
+      if (response.acknowledgement) {
+        toast.success(response.description);
+        setLoading(false);
+        event.target.reset();
+        setTimeout(() => {
+          navigate("/sign-in");
+        }, 2000);
+      } else {
+        toast.error(response.description);
+        setLoading(false);
+      }
+    };
+    resetPassword();
+  }
+
   return (
     <section className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
       <Title>Forgot password</Title>
@@ -20,26 +58,33 @@ const Forgot = () => {
             </h1>
             <div className="w-full flex-1">
               <div className="mx-auto max-w-xs mt-8">
-                <form className="flex flex-col gap-y-4">
-                  {/* email field */}
-                  <input
-                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                  />
+                {loading ? (
+                  <Loading />
+                ) : (
+                  <form
+                    className="flex flex-col gap-y-4"
+                    onSubmit={handleResetPassword}
+                  >
+                    {/* email field */}
+                    <input
+                      className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                    />
 
-                  {/* password field */}
-                  <input
-                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                    type="password"
-                    name="password"
-                    placeholder="New Password"
-                  />
+                    {/* password field */}
+                    <input
+                      className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                      type="password"
+                      name="password"
+                      placeholder="New Password"
+                    />
 
-                  {/* submit button */}
-                  <AccountButton value={"Forgot Password"} />
-                </form>
+                    {/* submit button */}
+                    <AccountButton value={"Forgot Password"} />
+                  </form>
+                )}
                 <p className="mt-6 text-xs text-gray-600 text-center">
                   Remember your account password?
                   <Link
