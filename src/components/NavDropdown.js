60b2647c6a56281186d31951a045a9dc8a7ce474
hasibulislam999 <hasib143sl@gmail.com>
2022-11-21 08:53:30 +0600

    modify style on dropdown block

--- src/components/NavDropdown.js
+++ src/components/NavDropdown.js
@@ -10,7 +10,7 @@ const NavDropdown = () => {
 
   return (
     <section
-      className="absolute bg-white w-full lg:mt-4 lg:ml-0 lg:top-full md:top-0 md:mt-0 lg:left-0 md:ml-2 md:left-full top-full mt-2 left-0 p-6 rounded-box z-50"
+      className="absolute bg-white w-full lg:mt-4 lg:ml-0 lg:top-full md:top-0 md:mt-0 lg:left-0 md:ml-2 md:left-full top-full mt-2 left-0 p-6 rounded-box z-50 shadow-lg border"
       onMouseLeave={() => {
         setDropdownState(false);
         setSelectDropdownState("");
