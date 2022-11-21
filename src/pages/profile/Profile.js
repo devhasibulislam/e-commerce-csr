aee77b8e222d1fa9e36eb6392a701fbe4db52871
hasibulislam999 <hasib143sl@gmail.com>
2022-11-21 22:00:43 +0600

    implement dynamic tab functionality

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import SecondaryContainer from "../../components/container/SecondaryContainer";
+import EditProfile from "../../components/profile/EditProfile";
 import Title from "../../components/Title";
 import Footer from "../../shared/footer/Footer";
 import Navbar from "../../shared/navbar/Navbar";
 import useMyself from "../../utilities/useMyself";
+
+export const UserContext = React.createContext({});
 
 const Profile = () => {
   const [tabState, setTabState] = useState("Account info");
@@ -19,31 +22,39 @@ const Profile = () => {
 
   return (
     <>
-      <Title>Profile</Title>
-      <Navbar />
-      <SecondaryContainer>
-        <div className="flex flex-col gap-y-8">
-          <h1 className="text-6xl underline font-medium">Account</h1>
-          <h2 className="text-5xl">
-            Welcome,{" "}
-            <span className="text-primary font-serif">{user?.name}</span>
-          </h2>
-        </div>
-        <div className="tabs grid lg:block md:grid-cols-4 grid-cols-3 lg:gap-0 gap-4 my-12">
-          {tabs.map((tab, index) => (
-            <span
-              key={index}
-              className={`tab tab-bordered whitespace-nowrap ${
-                tabState === tab && "tab-active"
-              }`}
-              onClick={() => setTabState(tab)}
-            >
-              {tab}
-            </span>
-          ))}
-        </div>
-      </SecondaryContainer>
-      <Footer />
+      <UserContext.Provider value={{ user, loading }}>
+        <Title>Profile</Title>
+        <Navbar />
+        <SecondaryContainer>
+          <div className="flex flex-col gap-y-8">
+            <h1 className="text-6xl underline font-medium">Account</h1>
+            <h2 className="text-5xl">
+              Welcome,{" "}
+              <span className="text-primary font-serif">{user?.name}</span>
+            </h2>
+          </div>
+          <div className="tabs grid lg:block md:grid-cols-4 grid-cols-3 lg:gap-0 gap-4 my-12">
+            {tabs.map((tab, index) => (
+              <span
+                key={index}
+                className={`tab tab-bordered whitespace-nowrap ${
+                  tabState === tab && "tab-active"
+                }`}
+                onClick={() => setTabState(tab)}
+              >
+                {tab}
+              </span>
+            ))}
+          </div>
+
+          {tabState === "Account info" && <EditProfile />}
+          {tabState === "Saved lists" && <>Saved lists</>}
+          {tabState === "Change password" && <>Change password</>}
+          {tabState === "Change billing" && <>Change billing</>}
+          {tabState === "Delete account" && <>Delete account</>}
+        </SecondaryContainer>
+        <Footer />
+      </UserContext.Provider>
     </>
   );
 };
