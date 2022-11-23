d3960c0efa61276e3b250f315abc0843007e224b
hasibulislam999 <hasib143sl@gmail.com>
2022-11-23 09:53:15 +0600

    implement auth guard on some routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Blogs from "./pages/blogs/Blogs";
 import Categories from "./pages/categories/Categories";
@@ -14,57 +14,72 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import useMyself from "./utilities/useMyself";
 import Loading from "./shared/loading/Loading";
+import RequireAuth from "./components/profile/RequireAuth";
+
+export const UserContext = React.createContext({});
 
 function App() {
   const [user, loading] = useMyself(localStorage?.getItem("accessToken"));
 
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
-
   return loading ? (
     <Loading />
   ) : (
-    <div className="App">
-      <Routes>
-        {/* home */}
-        <Route path="/" element={<Home />} />
+    <UserContext.Provider value={{ user, loading }}>
+      <div className="App">
+        <Routes>
+          {/* home */}
+          <Route path="/" element={<Home />} />
 
-        {/* dashboard */}
-        <Route path="/dashboard" element={<Dashboard />} />
+          {/* dashboard */}
+          <Route
+            path="/dashboard"
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+          />
 
-        {/* categories */}
-        <Route path="/categories" element={<Categories />} />
+          {/* categories */}
+          <Route path="/categories" element={<Categories />} />
 
-        {/* blogs */}
-        <Route path="/blogs" element={<Blogs />} />
+          {/* blogs */}
+          <Route path="/blogs" element={<Blogs />} />
 
-        {/* profile */}
-        <Route path="/profile" element={<Profile />} />
+          {/* profile */}
+          <Route
+            path="/profile"
+            element={
+              <RequireAuth>
+                <Profile />
+              </RequireAuth>
+            }
+          />
 
-        {/* account */}
-        <Route path="/sign-in" element={<Signin />} />
-        <Route path="/sign-up" element={<Signup />} />
-        <Route path="/reset-password" element={<Forgot />} />
+          {/* account */}
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/reset-password" element={<Forgot />} />
 
-        {/* not found */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          {/* not found */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
-      {/* alert toast */}
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </div>
+        {/* alert toast */}
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </div>
+    </UserContext.Provider>
   );
 }
 
