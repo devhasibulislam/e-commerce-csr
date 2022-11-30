338b53b1db9bb39eb9c751e85ef95667f1e4fe28
hasibulislam999 <hasib143sl@gmail.com>
2022-11-30 10:02:30 +0600

    new reference added

--- public/reference.js
+++ public/reference.js
@@ -22,4 +22,7 @@
  *
  * TypeError: split is not a function in JavaScript
  * https://bobbyhadz.com/blog/javascript-typeerror-string-split-is-not-a-function#:~:text=The%20%22split%20is%20not%20a,the%20split%20method%20on%20strings.
+ * 
+ * How to Add to an Array in React State using Hooks
+ * https://javascript.plainenglish.io/how-to-add-to-an-array-in-react-state-3d08ddb2e1dc
  */
