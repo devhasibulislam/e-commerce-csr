737af0616d4d97d33b77bc4495ec5f497c274b52
hasibulislam999 <hasib143sl@gmail.com>
2022-11-30 10:02:58 +0600

    route init

---
+++ src/pages/dashboard/products/ManageProducts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../../../components/Title";
+
+const ManageProducts = () => {
+  return (
+    <>
+      <Title>Manage New Products</Title>
+    </>
+  );
+};
+
+export default ManageProducts;
