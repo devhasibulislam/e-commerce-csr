fcc9679c3131b2f710fa24f409bb77f81c547637
hasibulislam999 <hasib143sl@gmail.com>
2022-12-01 19:49:55 +0600

    convert from regular fetching to react query

--- src/utilities/useBrands.js
+++ src/utilities/useBrands.js
@@ -1,26 +1,15 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 function useBrands() {
-  const [brands, setBrands] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ["brands"],
+    queryFn: () =>
+      fetch("https://e-commerce-ssr.onrender.com/brand").then((res) =>
+        res.json()
+      ),
+  });
 
-  useEffect(() => {
-    const getBrands = async () => {
-      setLoading(true);
-      const request = await fetch(`https://e-commerce-ssr.onrender.com/brand`);
-      const response = await request.json();
-      if (response.acknowledgement) {
-        setBrands(response.data);
-        setLoading(false);
-      } else {
-        console.log(response.description);
-        setLoading(false);
-      }
-    };
-    getBrands();
-  }, []);
-
-  return [brands, loading];
+  return { brands: data?.data, loading: isLoading, refetch };
 }
 
 export default useBrands;
