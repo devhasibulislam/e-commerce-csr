c90a9595c95c27fedd50f7c462ba850fe4cb5d09
hasibulislam999 <hasib143sl@gmail.com>
2022-12-01 07:41:10 +0600

    modal init

---
+++ src/components/Modal.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Modal = ({ showModal, setShowModal, modalHeader, content }) => {
+  return (
+    <>
+      {showModal && (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl lg:px-0 px-4">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">{modalHeader}</h3>
+                  <button
+                    className="btn btn-sm btn-circle btn-error"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 font-bold text-white"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  {content}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Modal;
