cd266b78fecc2b65036966c7e4566a48045ce956
hasibulislam999 <hasib143sl@gmail.com>
2022-12-01 23:32:35 +0600

    add new brand route init

---
+++ src/pages/dashboard/brands/AddNewBrand.js
@@ -0,0 +1,198 @@
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import Button from "../../../components/Button";
+import Title from "../../../components/Title";
+import TinyLoading from "../../../shared/loading/TinyLoading";
+
+const AddNewBrand = () => {
+  const [thumbnailsLoading, setThumbnailsLoading] = useState(false);
+  const [logo, setLogo] = useState({});
+  const [successfulState, setSuccessfulState] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  function handleAddNewBrand(event) {
+    event.preventDefault();
+
+    const brandInfo = {
+      title: event.target.brand.value,
+      email: event.target.email.value,
+      website: event.target.url.value,
+      description: event.target.description.value,
+      logo: logo,
+      location: event.target.location.value,
+    };
+
+    const insertNewBrand = async () => {
+      setLoading(true);
+      const request = await fetch(`http://localhost:8080/brand`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify(brandInfo),
+      });
+      const response = await request.json();
+      if (response.acknowledgement) {
+        event.target.reset();
+        toast.success(response.description);
+        setLoading(false);
+      } else {
+        toast.error(response.description);
+        setLoading(false);
+      }
+    };
+    insertNewBrand();
+  }
+
+  function handleBrandLogo(event) {
+    const formData = new FormData();
+    formData.append("logo", event.target.files[0]);
+
+    const uploadThumbnail = async () => {
+      setThumbnailsLoading(true);
+      const request = await fetch(`http://localhost:8080/brand/logo`, {
+        method: "POST",
+        body: formData,
+      });
+      const response = await request.json();
+      if (response?.acknowledgement) {
+        setLogo({
+          url: response?.data?.path,
+          public_id: response?.data?.filename,
+        });
+        toast.success(response.description);
+        setSuccessfulState(true);
+        setThumbnailsLoading(false);
+      } else {
+        toast.error(response.description);
+        setThumbnailsLoading(false);
+      }
+    };
+    uploadThumbnail();
+  }
+
+  return (
+    <>
+      <Title>Add New Brand</Title>
+      <form
+        className="flex flex-col gap-y-4 w-full lg:w-3/4"
+        onSubmit={handleAddNewBrand}
+      >
+        {/* brand title */}
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Brand Title</span>
+          </label>
+          <input
+            type="text"
+            name="brand"
+            placeholder="Min length 5 & Max length 50"
+            className="input input-bordered w-full lg:w-3/4"
+          />
+        </div>
+
+        {/* brand email */}
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Brand Email</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Provide brand email"
+            className="input input-bordered w-full lg:w-3/4"
+          />
+        </div>
+
+        {/* brand website */}
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Brand Website</span>
+          </label>
+          <input
+            type="url"
+            name="url"
+            placeholder="Enter an https:// URL:"
+            pattern="https://.*"
+            size={"30"}
+            className="input input-bordered w-full lg:w-3/4"
+          />
+        </div>
+
+        {/* brand description */}
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Brand Description</span>
+          </label>
+          <textarea
+            name="description"
+            placeholder="Min length 10 & Max length 250"
+            className="textarea textarea-bordered w-full lg:w-3/4"
+          />
+        </div>
+
+        {/* brand location */}
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Brand Location</span>
+          </label>
+          <input
+            type="text"
+            name="location"
+            placeholder="Min length 5 & Max length 50"
+            className="input input-bordered w-full lg:w-3/4"
+          />
+        </div>
+
+        {/* brand logo */}
+        {thumbnailsLoading === true ? (
+          <div className="w-full lg:w-3/4">
+            <TinyLoading />
+          </div>
+        ) : (
+          <div className="form-control">
+            {successfulState === true ? (
+              <>
+                <div className="alert alert-success shadow-lg w-full lg:w-3/4 rounded-lg">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="stroke-current flex-shrink-0 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                    <span>Your logo uploaded successfully!</span>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <label className="label">
+                  <span className="label-text">Logo must be {"<="} 1MB</span>
+                </label>
+                <input
+                  type="file"
+                  name="logo"
+                  className="file-input file-input-bordered w-full lg:w-3/4"
+                  onChange={handleBrandLogo}
+                />
+              </>
+            )}
+          </div>
+        )}
+
+        {loading ? <TinyLoading /> : <Button>Add new brand</Button>}
+      </form>
+    </>
+  );
+};
+
+export default AddNewBrand;
