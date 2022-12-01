40f1078cf4e0df5cac7016c25423166bdaf6ba9f
hasibulislam999 <hasib143sl@gmail.com>
2022-12-01 19:38:02 +0600

    adding header gap

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,7 +10,7 @@ const Modal = ({ showModal, setShowModal, modalHeader, content }) => {
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                <div className="flex items-start justify-between gap-x-8 p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">{modalHeader}</h3>
                   <button
                     className="btn btn-sm btn-circle btn-error"
