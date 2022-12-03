9a0987fb8da213b25afc15b8db73db98e2206045
hasibulislam999 <hasib143sl@gmail.com>
2022-12-03 22:52:35 +0600

    integrate context API to open modal for specific product

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import ProductOverview from "../components/dashboard/product/ProductOverview";
 import Banner from "../components/home/Banner";
 import BestSelling from "../components/home/BestSelling";
 import Blogs from "../components/home/Blogs";
@@ -13,35 +14,57 @@ import Reviews from "../components/home/Reviews";
 import StartExploring from "../components/home/StartExploring";
 import Trending from "../components/home/Trending";
 import WorkProcedure from "../components/home/WorkProcedure";
+import Modal from "../components/Modal";
 import ScrollToTop from "../components/ScrollToTop";
 import Title from "../components/Title";
 import Footer from "../shared/footer/Footer";
 import Navbar from "../shared/navbar/Navbar";
 
+// context API
+export const OpenOverviewModal = React.createContext(false);
+
 const Home = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [product, setProduct] = useState({});
+
   return (
-    <section id="home">
-      <Title>Home</Title>
-      <Navbar />
-      <Banner />
-      <Discover />
-      <NewArrival />
-      <WorkProcedure />
-      <hr className="container mx-auto" />
-      <Motivation />
-      <StartExploring />
-      <BestSelling />
-      <Promo />
-      <ExpertsChoice />
-      <hr className="container mx-auto" />
-      <Department />
-      <Newsletter />
-      <Trending />
-      <Blogs />
-      <Reviews />
-      <Footer />
-      <ScrollToTop />
-    </section>
+    <>
+      <OpenOverviewModal.Provider
+        value={{ openModal, setOpenModal, setProduct }}
+      >
+        <section id="home">
+          <Title>Home</Title>
+          <Navbar />
+          <Banner />
+          <Discover />
+          <NewArrival />
+          <WorkProcedure />
+          <hr className="container mx-auto" />
+          <Motivation />
+          <StartExploring />
+          <BestSelling />
+          <Promo />
+          <ExpertsChoice />
+          <hr className="container mx-auto" />
+          <Department />
+          <Newsletter />
+          <Trending />
+          <Blogs />
+          <Reviews />
+          <Footer />
+          <ScrollToTop />
+        </section>
+      </OpenOverviewModal.Provider>
+
+      {openModal && (
+        <Modal
+          showModal={openModal}
+          setShowModal={setOpenModal}
+          modalHeader={"Product Overview"}
+          content={<ProductOverview product={product} />}
+        />
+      )}
+    </>
   );
 };
 
