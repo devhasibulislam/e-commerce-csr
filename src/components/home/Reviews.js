ba4453f19c2a85b067a6eed25f9de76071f081c3
hasibulislam999 <hasib143sl@gmail.com>
2022-12-04 00:38:24 +0600

    refactor with proper data attachment

--- src/components/home/Reviews.js
+++ src/components/home/Reviews.js
@@ -4,74 +4,11 @@ import GreyText from "../GreyText";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import ReviewCard from "../cards/ReviewCard";
+import useReviews from "../../utilities/useReviews";
+import SmallLoading from "../../shared/loading/SmallLoading";
 
 const Reviews = () => {
-  // carousel data
-  const reviews = [
-    {
-      user: {
-        name: "Tina Abie",
-        designation: "Designer",
-        avatar: "/assets/review/review1.png",
-      },
-      detail:
-        "Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland",
-    },
-    {
-      user: {
-        name: "Tina Abie",
-        designation: "Designer",
-        avatar: "/assets/review/review2.png",
-      },
-      detail:
-        "Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland",
-    },
-    {
-      user: {
-        name: "Tina Abie",
-        designation: "Designer",
-        avatar: "/assets/review/review3.png",
-      },
-      detail:
-        "Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland",
-    },
-    {
-      user: {
-        name: "Tina Abie",
-        designation: "Designer",
-        avatar: "/assets/review/review4.png",
-      },
-      detail:
-        "Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland",
-    },
-    {
-      user: {
-        name: "Tina Abie",
-        designation: "Designer",
-        avatar: "/assets/review/review5.png",
-      },
-      detail:
-        "Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland",
-    },
-    {
-      user: {
-        name: "Tina Abie",
-        designation: "Designer",
-        avatar: "/assets/review/review6.png",
-      },
-      detail:
-        "Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland",
-    },
-    {
-      user: {
-        name: "Tina Abie",
-        designation: "Designer",
-        avatar: "/assets/review/review7.png",
-      },
-      detail:
-        "Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland",
-    },
-  ];
+  const { reviews, loading } = useReviews();
 
   // carousel configure
   const responsive = {
@@ -99,7 +36,7 @@ const Reviews = () => {
     <PrimaryContainer>
       <h1 className="lg:text-4xl md:text-2xl text-xl font-semibold mb-4">
         Good news from far away.{" "}
-        <GreyText>Let's see what people think of Ciseco</GreyText>
+        <GreyText>Let's see what people think of Canim</GreyText>
       </h1>
       <Carousel
         responsive={responsive}
@@ -109,9 +46,13 @@ const Reviews = () => {
         infinite={true}
         className="py-4 discover_carousel"
       >
-        {reviews?.map((review, index) => (
-          <ReviewCard key={index} review={review} />
-        ))}
+        {loading ? (
+          <SmallLoading />
+        ) : (
+          reviews?.map((review, index) => (
+            <ReviewCard key={index} review={review} />
+          ))
+        )}
       </Carousel>
     </PrimaryContainer>
   );
