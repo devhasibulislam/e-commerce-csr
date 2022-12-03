bf49ad3ca5dfc609c6a8cc34b86c89b3658ef1ac
hasibulislam999 <hasib143sl@gmail.com>
2022-12-03 22:51:40 +0600

    display categories

--- src/components/NavDropdown.js
+++ src/components/NavDropdown.js
@@ -1,22 +1,43 @@
 import React, { useContext } from "react";
+import TinyLoading from "../shared/loading/TinyLoading";
 import { ItemContext } from "../shared/navbar/Navbar";
+import useCategories from "../utilities/useCategories";
 
 const NavDropdown = () => {
-  const {
-    setDropdownState,
-    selectDropdownState: category,
-    setSelectDropdownState,
-  } = useContext(ItemContext);
+  const { categories, loading } = useCategories();
+
+  const { setDropdownState, setSelectDropdownState } = useContext(ItemContext);
 
   return (
     <section
-      className="absolute bg-white w-full lg:mt-4 lg:ml-0 lg:top-full md:top-0 md:mt-0 lg:left-0 md:ml-2 md:left-full top-full mt-2 left-0 p-6 rounded-box z-50 shadow-lg border"
+      className="absolute bg-white w-full lg:mt-4 lg:ml-0 lg:top-full md:top-0 md:mt-0 lg:left-0 md:ml-2 md:left-full top-full mt-2 left-0 rounded-box z-50 shadow-lg border h-30 overflow-y-scroll flex flex-col gap-y-4"
       onMouseLeave={() => {
         setDropdownState(false);
         setSelectDropdownState("");
       }}
     >
-      You are choosing {category} category
+      {loading ? (
+        <TinyLoading />
+      ) : (
+        categories?.map((category) => (
+          <div
+            key={category?._id}
+            className="flex flex-row gap-x-2 w-full px-4 py-2 hover:shadow cursor-pointer hover:bg-slate-100"
+          >
+            <img
+              src={category?.thumbnail?.url}
+              alt={category?.thumbnail?.public_id}
+              className="w-[40px] h-[40px] object-cover rounded-full"
+            />
+            <article className="flex flex-col gap-y-1">
+              <h2 className="font-medium capitalize">{category?.title}</h2>
+              <p className="text-gray-500 capitalize text-left" title={category.description}>
+                {category.description.slice(0, 30) + "..."}
+              </p>
+            </article>
+          </div>
+        ))
+      )}
     </section>
   );
 };
