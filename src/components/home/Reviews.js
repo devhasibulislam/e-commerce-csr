dbbde2816acb83d2442d73c08c0dd45520fc6f15
hasibulislam999 <hasib143sl@gmail.com>
2022-12-03 22:51:10 +0600

    review viewport changed

--- src/components/home/Reviews.js
+++ src/components/home/Reviews.js
@@ -77,17 +77,17 @@ const Reviews = () => {
   const responsive = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 3000 },
-      items: 2,
+      items: 3,
       partialVisibilityGutter: 40,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 2,
+      items: 3,
       partialVisibilityGutter: 40,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 1,
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
