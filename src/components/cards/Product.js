04793a2c2c1553c68cab677e786ea9dc4cdfa68f
hasibulislam999 <hasib143sl@gmail.com>
2022-12-04 08:31:28 +0600

    implement favorites feature

--- src/components/cards/Product.js
+++ src/components/cards/Product.js
@@ -1,9 +1,22 @@
 import React, { useContext } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { OpenOverviewModal } from "../../pages/Home";
+import {
+  addToFavorites,
+  removeFromFavorites,
+} from "../../utilities/useFavorite";
 import useRandomInt from "../../utilities/useRandomInt";
 
 const Product = ({ product }) => {
   const { openModal, setOpenModal, setProduct } = useContext(OpenOverviewModal);
+  const [favoritesId, setFavoritesId] = useState([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      setFavoritesId(localStorage?.getItem("favorites"));
+    }, 1000);
+  }, []);
 
   return (
     <section className="lg:p-6 md:p-6 p-2 hover:shadow rounded-xl flex flex-col gap-y-4 relative">
@@ -17,15 +30,51 @@ const Product = ({ product }) => {
             className="h-[300px] w-full object-cover rounded-xl"
           />
         ))}
-        <span
-          className="tooltip tooltip-left tooltip-secondary absolute top-6 right-6 shadow rounded-full"
-          data-tip="Brief overview"
+        {/* add to favorites */}
+        {favoritesId?.includes(product?._id) ? (
+          <button
+            class="w-9 h-9 flex items-center justify-center rounded-full shadow btn-ghost nc-shadow-lg absolute top-6 right-16 z-10"
+            onClick={() => removeFromFavorites(product._id)}
+          >
+            <span className="tooltip" data-tip="Added to favorites">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6 text-red-600"
+              >
+                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+              </svg>
+            </span>
+          </button>
+        ) : (
+          <button
+            class="w-9 h-9 flex items-center justify-center rounded-full shadow btn-ghost nc-shadow-lg absolute top-6 right-16 z-10"
+            onClick={() => addToFavorites(product._id)}
+          >
+            <span className="tooltip" data-tip="Add to favorites">
+              <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M12.62 20.81C12.28 20.93 11.72 20.93 11.38 20.81C8.48 19.82 2 15.69 2 8.68998C2 5.59998 4.49 3.09998 7.56 3.09998C9.38 3.09998 10.99 3.97998 12 5.33998C13.01 3.97998 14.63 3.09998 16.44 3.09998C19.51 3.09998 22 5.59998 22 8.68998C22 15.69 15.52 19.82 12.62 20.81Z"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            </span>
+          </button>
+        )}
+        {/* brief overview */}
+        <button
+          class="w-9 h-9 flex items-center justify-center rounded-full shadow btn-ghost nc-shadow-lg absolute top-6 right-6 z-10"
           onClick={() => {
             setOpenModal(!openModal);
             setProduct(product);
           }}
         >
-          <span className="btn btn-sm btn-ghost btn-circle rounded-full">
+          <span className="tooltip" data-tip="Brief Overview">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
@@ -40,7 +89,7 @@ const Product = ({ product }) => {
               />
             </svg>
           </span>
-        </span>
+        </button>
       </div>
       <div className="flex flex-col gap-y-2">
         <h2 className="whitespace-nowrap text-ellipsis overflow-hidden font-medium">
@@ -88,11 +137,17 @@ const Product = ({ product }) => {
             <span className="btn btn-sm btn-primary btn-circle text-white rounded-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
               >
-                <path d="M1 1.75A.75.75 0 011.75 1h1.628a1.75 1.75 0 011.734 1.51L5.18 3a65.25 65.25 0 0113.36 1.412.75.75 0 01.58.875 48.645 48.645 0 01-1.618 6.2.75.75 0 01-.712.513H6a2.503 2.503 0 00-2.292 1.5H17.25a.75.75 0 010 1.5H2.76a.75.75 0 01-.748-.807 4.002 4.002 0 012.716-3.486L3.626 2.716a.25.25 0 00-.248-.216H1.75A.75.75 0 011 1.75zM6 17.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM15.5 19a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
               </svg>
             </span>
           </span>
