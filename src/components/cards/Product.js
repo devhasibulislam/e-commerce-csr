e520e87ec323a591e3fd5e9590422fd9eea33d4c
hasibulislam999 <hasib143sl@gmail.com>
2022-12-04 22:54:24 +0600

    favorites conditionalized

--- src/components/cards/Product.js
+++ src/components/cards/Product.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { OpenOverviewModal } from "../../pages/Home";
 import {
   addToFavorites,
@@ -11,6 +12,9 @@ import useRandomInt from "../../utilities/useRandomInt";
 const Product = ({ product }) => {
   const { openModal, setOpenModal, setProduct } = useContext(OpenOverviewModal);
   const [favoritesId, setFavoritesId] = useState([]);
+
+  const location = useLocation();
+  console.log(location);
 
   useEffect(() => {
     setInterval(() => {
@@ -67,29 +71,31 @@ const Product = ({ product }) => {
           </button>
         )}
         {/* brief overview */}
-        <button
-          class="w-9 h-9 flex items-center justify-center rounded-full shadow btn-ghost nc-shadow-lg absolute top-6 right-6 z-10"
-          onClick={() => {
-            setOpenModal(!openModal);
-            setProduct(product);
-          }}
-        >
-          <span className="tooltip" data-tip="Brief Overview">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-5 h-5"
-            >
-              <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
-              <path
-                fillRule="evenodd"
-                d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </span>
-        </button>
+        {location?.pathname === "/profile" ? <></> : (
+          <button
+            class="w-9 h-9 flex items-center justify-center rounded-full shadow btn-ghost nc-shadow-lg absolute top-6 right-6 z-10"
+            onClick={() => {
+              setOpenModal(!openModal);
+              setProduct(product);
+            }}
+          >
+            <span className="tooltip" data-tip="Brief Overview">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
+                <path
+                  fillRule="evenodd"
+                  d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </button>
+        )}
       </div>
       <div className="flex flex-col gap-y-2">
         <h2 className="whitespace-nowrap text-ellipsis overflow-hidden font-medium">
