69d2bc9dc54b427f9ffb6f7a4b912fc2830fcc2e
hasibulislam999 <hasib143sl@gmail.com>
2022-12-06 21:01:55 +0600

    capitalized font

--- src/shared/navbar/Cart.js
+++ src/shared/navbar/Cart.js
@@ -55,7 +55,7 @@ const Cart = () => {
           <span className="font-bold text-lg">8 Items</span>
           <span className="text-info">Subtotal: $999</span>
           <div className="card-actions">
-            <button className="btn btn-primary btn-block">View cart</button>
+            <button className="btn btn-primary btn-block capitalize">View cart</button>
           </div>
         </div>
       </div>
